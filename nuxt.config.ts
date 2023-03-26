f1d5d07bae4a1e8196b8f18b19679f2379b94af6
Francisco Ramos <fro.profesional@gmail.com>
2023-03-26 02:28:13 -0700

    Remove commented code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ['@clau-org/ui-configuration'],
+  extends: [
+    '@clau-org/ui-configuration'
+  ],
   srcDir: 'src/',
-
-  // tailwindcss: {
-  //   config: {
-  //     content: ["src/**/**.vue"],
-  //   },
-  // },
 })
