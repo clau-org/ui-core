569c9ce7fa86549d249c43a97e2cd711cb40f911
Francisco Ramos <fro.profesional@gmail.com>
2023-03-26 00:53:10 -0700

    Change usage of module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ['@clau-org/ui-core'],
+  extends: ['@clau-org/ui-configuration'],
   srcDir: 'src/',
 
   // tailwindcss: {
